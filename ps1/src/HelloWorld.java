e4c0758350f7b6e8e0c5d888c3837e91e20f1e11
freddychenyouren2 <freddychenyouren2@gmail.com>
2023-04-19 00:26:51 +0800

    Adding PS1 into personal GitHub account

---
+++ ps1/src/HelloWorld.java
@@ -0,0 +1,31 @@
+
+public class HelloWorld {
+    public static void main(String args[]) {
+        int age = 19;
+        String name = "Freddy Chen You Ren";
+        String country = "Indonesia";
+
+        //Introduction
+        System.out.printf("Hello! My name is " + name + " and I am from " + country + ".%n" +
+                "I have been studying in Singapore for 5 years (Entering at Secondary 3).%n%n" +
+                "I am Interested in how Mathematics, Science, and Computing can improve our world and would surely like to contribute to that!%n" +
+                "In my free time, I play games that contain strategy aspects, play or listen to music, or browse current affairs as much as I can.%n" +
+                "I am also an MRT enthusiast, observing almost everything about Singapore MRT such as the entire System Map up till Thomson East Coast Line Stage 3, exterior and interior designs, engine sounds and MRT speed, acceleration, how MRTs in interchanges arrive during peak and off-peak hours, how many beeps are there before the door closes for each model of MRTs, and so many more!%n" +
+                "In this CS2040S Class, I would like to learn more on efficient ways to solve real-life problems that we may face in the future, both professionally and personally.%n" +
+                "I also want to forge as many meaningful relationships as possible with the cohort! So that we may all have fun and excel together!%n%n" +
+
+                "Joke #1: 'Sometimes... When I close my eye in the middle of the night... I see nothing'%n%n" +
+                "Joke #2: POV: When you learn Chemical Formulae online and come for a class:%n" +
+                "Student: Teacher! Can I know what is the formula for Sodium?%n" +
+                "Teacher: Na%n" +
+                "Student: O...k...? What about Sodium Hydride? %n" +
+                "Teacher: NaH%n" +
+                "Student: ??? (Why does he not want to tell me?! Nevermind I'll ask someone else)%n" +
+                "Student: (Turns over to a classmate for help) Yo bro, need your help, can you tell me what is the formula for Sodium Hypobromite?%n" +
+                "Classmate: NaBro%n" +
+                "Student: (Walks out of class)%n%n%n");
+
+        //Answers to Problems 1a and 1b
+        System.out.println("Problem 1a answer: 3125. %n" + "Problem 1b answer: Calculates the value of argA raised to the power of argB.");
+    }
+}
